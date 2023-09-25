e421beced2af7d3b450fc8cec2329ed3e237f0f1
Chris Teski <12370776+cteski@users.noreply.github.com>
2023-09-25 15:25:09 -0400

    Correct grid constant

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,7 @@
 const mainContainerDiv = document.querySelector('#main-container');
 
 
-const GRID_DIV_MAX = 64;
+const GRID_DIV_MAX = 256;
 
 for (let i = 0; i < GRID_DIV_MAX; i++) {
     const gridDiv = document.createElement('div');

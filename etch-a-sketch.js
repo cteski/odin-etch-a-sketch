acea78c9edf56814f4bd8deb0449eddba87b3a10
Chris Teski <12370776+cteski@users.noreply.github.com>
2023-09-25 16:15:54 -0400

    Update css/js to fill in hovered divs

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,16 @@
 const mainContainerDiv = document.querySelector('#main-container');
 
-
 const GRID_DIV_MAX = 256;
 
 for (let i = 0; i < GRID_DIV_MAX; i++) {
     const gridDiv = document.createElement('div');
     mainContainerDiv.appendChild(gridDiv);
 }
+
+const gridDivs = mainContainerDiv.querySelectorAll('div');
+
+gridDivs.forEach((gridItem) => {
+    gridItem.addEventListener('mouseover', () => {
+        gridItem.classList.add('filled');
+    });
+});
